a8d47699464b44136d5b3ed29d4d10567c1be99b
Alamin <codepapa360@gmail.com>
2023-06-30 19:43:18 +0600

    Final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,42 @@
 "use strict";
 import "../sass/main.scss";
 
-const idEl = document.getElementById("advice-id");
-const adviceEl = document.getElementById("advice");
-const btnGenerate = document.getElementById("generate-btn");
-const spinner = document.querySelector(".spinner");
-const adviceContents = document.querySelector(".advice-contents");
+(function () {
+  const idEl = document.getElementById("advice-id");
+  const adviceEl = document.getElementById("advice");
+  const btnGenerate = document.getElementById("generate-btn");
+  const spinner = document.querySelector(".spinner");
+  const adviceContents = document.querySelector(".advice-contents");
 
-const apiCall = async function () {
-  const url = "https://api.adviceslip.com/advice?" + Date.now();
+  const apiCall = async function () {
+    const url = "https://api.adviceslip.com/advice?" + Date.now();
 
-  const data = await fetch(url).then((res) => res.json());
-  return data;
-};
+    const data = await fetch(url).then((res) => res.json());
+    return data;
+  };
 
-const caller = async function () {
-  spinner.style.display = "block";
-  adviceContents.style.display = "none";
-  adviceContents.classList.remove("show-contents");
+  const caller = async function () {
+    spinner.style.display = "block";
+    adviceContents.style.display = "none";
+    adviceContents.classList.remove("show-contents");
 
-  try {
-    const data = await apiCall();
+    try {
+      const data = await apiCall();
 
-    idEl.textContent = data.slip.id;
-    adviceEl.textContent = `"${data.slip.advice}"`;
-    adviceContents.style.display = "block";
+      idEl.textContent = data.slip.id;
+      adviceEl.textContent = `"${data.slip.advice}"`;
+      adviceContents.style.display = "block";
 
-    setTimeout(() => {
-      spinner.style.display = "none";
-      adviceContents.classList.add("show-contents");
-    }, 10);
-  } catch (error) {
-    console.error(error);
-    spinner.style.display = "none";
-  }
-};
-
-caller();
-
-btnGenerate.addEventListener("click", function () {
+      setTimeout(() => {
+        spinner.style.display = "none";
+        adviceContents.classList.add("show-contents");
+      }, 10);
+    } catch (error) {
+      console.error(error);
+      spinner.style.display = "block";
+    }
+  };
   caller();
-});
+
+  btnGenerate.addEventListener("click", caller);
+})();
